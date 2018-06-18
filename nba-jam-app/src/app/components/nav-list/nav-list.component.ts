4c322434d315b6f9b9c4fc2aaf996c9e6c24c92f
Kyle Morton <kylemortondevelopment@gmail.com>
2018-06-17 20:54:46 -0500

    navbar working as expected, changed theme

--- nba-jam-app/src/app/components/nav-list/nav-list.component.ts
+++ nba-jam-app/src/app/components/nav-list/nav-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav-list',
   templateUrl: './nav-list.component.html',
   styleUrls: ['./nav-list.component.css']
 })
-export class NavListComponent implements OnInit {
+export class NavListComponent {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 
-  ngOnInit() {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
